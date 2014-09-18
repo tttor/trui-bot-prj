88a19bd8c82abec633c4e0c31e3e78e749072868
Lintang Sutawika <lintang.adyuta@ui.ac.id>
2014-09-18 22:42:19 +0700

    removed dri bot-mdl/

--- bot-mdl/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/lintang-sutawika/KRAI/trui-bot-prj/bot-mdl/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/lintang-sutawika/KRAI/trui-bot-prj/bot-mdl/devel;/opt/ros/indigo".split(';') if "/home/lintang-sutawika/KRAI/trui-bot-prj/bot-mdl/devel;/opt/ros/indigo" != "" else []
--- bot-mdl/build/using_markers/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lintang-sutawika/KRAI/trui-bot-prj/bot-mdl/src/using_markers
-# Build directory: /home/lintang-sutawika/KRAI/trui-bot-prj/bot-mdl/build/using_markers
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
