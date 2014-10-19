c61c9183bf2b9bd88ecbbae72a51d49ef0350466
lomempow <emily.lomempow@gmail.com>
2014-10-19 20:07:18 +0700

    timer in slave needs to be checked on hardware first

--- robot/main/slave2.cc
+++ robot/main/slave2.cc
@@ -3,25 +3,54 @@
 #include <comm/custom.h>
 #include <actuator/motor.h>
 
+int timer1_counter;
+const size_t pwm_pin = 11;
+const size_t dir_pin = 12; 
+const float outmax = 100.0; 
+const float outmin = -100.0; 
+
+const int encoder_out_a_pin = 2;
+const int encoder_out_b_pin = 3;
+const int encoder_resolution = 360; //Only needed for Initialization, not used unless .rot() is called
+
+float speed;
+  
+trui::Motor motor(pwm_pin, dir_pin, encoder_out_a_pin, encoder_out_b_pin, encoder_resolution, outmax, outmin);
+
+void setup()
+{
+  
+  // initialize timer1 
+  noInterrupts();           // disable all interrupts
+  TCCR1A = 0;      // set entire TCCR1A register to 0
+  TCCR1B = 0;     // same for TCCR1B
+ 
+  // set compare match register to desired timer count:
+  OCR1A = 780;
+  // turn on CTC mode:
+  TCCR1B |= (1 << WGM12);
+  // Set CS10 and CS12 bits for 1024 prescaler:
+  TCCR1B |= (1 << CS10);
+  TCCR1B |= (1 << CS12);
+  // enable timer compare interrupt:
+  TIMSK1 |= (1 << OCIE1A);
+  interrupts();             // enable all interrupts
+}
+
+ISR(TIMER1_COMPA_vect)        // interrupt service routine 
+{
+  //interrupts();
+  motor.set_speed(speed);
+}
+
 int main() {
   init();// this needs to be called before setup() or some functions won't work there
-  
-  Serial.begin(57600);
-
-  const size_t pwm_pin = 11;
-  const size_t dir_pin = 12; 
-  const float outmax = 100.0; 
-  const float outmin = -100.0; 
-
-  const int encoder_out_a_pin = 2;
-  const int encoder_out_b_pin = 3;
-  const int encoder_resolution = 360; //Only needed for Initialization, not used unless .rot() is called
-  
-  trui::Motor motor(pwm_pin, dir_pin, encoder_out_a_pin, encoder_out_b_pin, encoder_resolution, outmax, outmin);
+  setup();
   motor.setup();
 
+  Serial.begin(57600);
+
   long timeNow = 0, timeOld = 0;
-  float x;
 
   while (true) {
     //1. Wait msg from master
@@ -50,8 +79,8 @@ int main() {
     if ( (rx_msg.msgid==set_speed_msgid) ) {
       mavlink_manual_setpoint_t msg;
       mavlink_msg_manual_setpoint_decode(&rx_msg, &msg);
-      float actual_speed= msg.roll;
-      x = motor.set_speed(actual_speed);
+      
+      speed= msg.roll;
     } 
     //2B. If msgid = asking for actual speed, send back actual speed to master
     else if ( (rx_msg.msgid==actual_speed_query_msgid) ) {
