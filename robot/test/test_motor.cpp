895429fa89a33e33d39bb1b43e61053f63d4f253
vektor dewanto <vektor.dewanto@gmail.com>
2014-10-25 11:00:22 +0700

    small fix

--- robot/test/test_motor.cpp
+++ robot/test/test_motor.cpp
@@ -2,7 +2,8 @@
 
 int main() {
   init();
-  Serial.begin(115200);
+  Serial.begin(9600);
+
   const size_t pwm_pin = 11;
   const size_t dir_pin = 12; 
   const float outmax = 100.0; 
@@ -13,18 +14,17 @@ int main() {
   const int encoder_resolution = 360; //Only needed for Initialization, not used unless .rot() is called
   
   trui::Motor motor(pwm_pin, dir_pin, encoder_out_a_pin, encoder_out_b_pin, encoder_resolution, outmax, outmin);
-  motor.setup();
 
   long timeNow = 0, timeOld = 0;
-  float x;
-  //motor.testing_encoder();
+  float cmd_speed = 50.0;
 
   while (true) {
     timeNow = millis();
     if(timeNow - timeOld > 50){
       timeOld = timeNow;
-      x = motor.set_speed(speed);
-      Serial.println(x);
+      motor.set_speed(cmd_speed);
+
+      Serial.print("cmd_speed= "); Serial.println(cmd_speed);
     }  
   }
 }
