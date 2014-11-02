3fdd6216b509742df73afdfa7cce875fd428aeb1
vektor dewanto <vektor.dewanto@gmail.com>
2014-11-02 15:38:30 +0700

    refine global planner, consider only 2D (x and y) though

--- controller/src/rbmt_move_base/include/rbmt_move_base/global_planner.h
+++ controller/src/rbmt_move_base/include/rbmt_move_base/global_planner.h
@@ -3,9 +3,11 @@
 
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <Eigen/Dense>
 
 #include <vector>
 #include <iostream>
+#include <cmath>
 
 namespace rbmt_move_base {
 class GlobalPlanner {
@@ -18,7 +20,20 @@ class GlobalPlanner {
   for now, it merely consists of 2 waypoints: start and goal poses
   */
   bool plan(const geometry_msgs::PoseStamped& start_pose, const geometry_msgs::PoseStamped& goal_pose, std::vector<geometry_msgs::PoseStamped>* global_plan);
+
  private:
+  template<typename T>
+  double get_cartesian_dist(const T& p1, const T& p2) {
+    const size_t n_dim = p1.size();
+
+    double total = 0.0;
+    for (size_t i=0; i<n_dim; ++i) {
+      double d = p1(i) - p2(i);
+      total += pow(d,2);
+    }
+      
+    return sqrt(total);
+  }
 };
 
 }// namespace rbmt_move_base
