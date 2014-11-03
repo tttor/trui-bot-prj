f744232dd9316008d59a6c239309a9a00a1d524c
vektor dewanto <vektor.dewanto@gmail.com>
2014-11-03 13:23:22 +0700

    fix msg type of cock pose

--- controller/src/rbmt_tracking/src/tracking_node.cpp
+++ controller/src/rbmt_tracking/src/tracking_node.cpp
@@ -8,12 +8,13 @@ int main(int argc, char** argv){
   ros::Publisher marker_pub;
   
   rbmt_tracking::Tracker tracker(nh);
-  // tracker.run(ros::Rate(10));
+  
 
   ROS_INFO("Waiting for nav stuff to be ready");
   ros::Duration(5.0).sleep();
 
-  tracker.run_dummy(ros::Rate(1.0));
+  // tracker.run_dummy(ros::Rate(1.0));
+  tracker.run(ros::Rate(10));
 
   return(0);
 }
