ee03c78933946ab3c5867bf8be5ee6ca1942f2bb
Lintang Sutawika <lintang.adyuta@ui.ac.id>
2015-03-23 19:18:28 +0700

    eddited master.cc to be a full fledge ros node

--- robot/src/ros_lib/geometry_msgs/Twist.h
+++ robot/src/ros_lib/geometry_msgs/Twist.h
@@ -4,8 +4,8 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
-#include "ros/msg.h"
-#include "geometry_msgs/Vector3.h"
+#include <ros_lib/ros/msg.h>
+#include <ros_lib/geometry_msgs/Vector3.h>
 
 namespace geometry_msgs
 {
